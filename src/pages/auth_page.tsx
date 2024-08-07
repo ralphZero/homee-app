6b131baabd38ae2c8b5709b68c08e49d7ce12604
Ralph Sébastien Placide <placideralph@gmail.com>
2024-08-06 22:39:19 -0400

    chore: Update image paths and imports in AuthPage component

--- src/pages/auth_page.tsx
+++ src/pages/auth_page.tsx
@@ -1,46 +1,20 @@
+import { HeroImage } from 'components/HeaderImage';
 import React from 'react';
 import { StyleSheet, View, SafeAreaView, Image, StatusBar } from 'react-native';
 
-const AuthPage = () => {
+export const AuthPage = () => {
   return (
     <View>
       <HeroImage />
       <SafeAreaView>
-        <Image style={styles.logo} source={require('../assets/logo.png')} />
+        <Image style={styles.logo} source={require('@assets/logo.png')} />
       </SafeAreaView>
       <StatusBar barStyle={'light-content'} />
     </View>
   );
 };
 
-const HeroImage = () => {
-  return (
-    <View style={styles.headerContainer}>
-      <Image
-        style={styles.imageShadow}
-        source={require('../assets/shadow.png')}
-      />
-      <Image
-        style={styles.heroImage}
-        source={require('../assets/hero_image.png')}
-      />
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
-  headerContainer: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-  },
-  imageShadow: {
-    position: 'absolute',
-    zIndex: 2,
-  },
-  heroImage: {
-    width: '100%',
-  },
   logo: {
     height: 60,
     alignSelf: 'center',
@@ -48,5 +22,3 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 });
-
-export default AuthPage;
